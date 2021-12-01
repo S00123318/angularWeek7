913555e499024bed918b7e6c566a8679b2818d42
Dale <S00123318@mail.itsligo.ie>
2021-12-01 12:37:06 +0000

    updated

--- clientApp2021/src/app/book2/book-list/book-list.component.ts
+++ clientApp2021/src/app/book2/book-list/book-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import{Book} from'../../book';
-import { BookService } from 'src/app/book.service';
+import { Book } from '../../book'
+import { BookService } from '../../book.service'
+
+
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
@@ -8,28 +10,135 @@ import { BookService } from 'src/app/book.service';
 })
 export class BookListComponent implements OnInit {
 
-  testString= 'hello'
-  testArray=['a','b','c']
-  books: Book[]=[]
   bookList: Book[] = [];
   message: string = "";
+  showBookForm: boolean = false;
 
-  currentBook! : Book;
-
+  currentBook?: Book = undefined;
 
   constructor(private bookService: BookService) { }
 
   ngOnInit(): void {
 
+
     this.bookService.getBooks().subscribe({
-      next: (value: Book[] )=> this.bookList = value,
-      complete: () => console.log(' service finished'),
+      next: (value: Book[]) => this.bookList = value,
+      complete: () => console.log('book service finished'),
+      error: (mess) => this.message = mess
+    })
+  }
+
+  clicked(book: Book): void {
+    this.currentBook = book;
+    console.table(this.currentBook)
+  }
+
+  isSelected(book: Book): boolean {
+    if (!book || !this.currentBook) {
+      return false;
+    }
+    else {
+
+      return book._id === this.currentBook._id;
+    }
+  }
+
+  openAddBook(): void {
+    this.currentBook = undefined;
+    this.showBookForm = true;
+  }
+
+  openEditBook(): void {
+    this.showBookForm = true;
+  }
+
+  addNewBook(newBook: Book): void {
+    console.log('adding new book ' + JSON.stringify(newBook));
+    this.bookService.addBook({ ...newBook })
+      .subscribe({
+        next: book => {
+          console.log(JSON.stringify(book) + ' has been added');
+          this.message = "new book has been added";
+        },
+        error: (err) => this.message = err
+      });
+
+    // so the updated list appears
+
+    this.bookService.getBooks().subscribe({
+      next: (value: Book[]) => this.bookList = value,
+      complete: () => console.log('book service finished'),
+      error: (mess) => this.message = mess
+    })
+  }
+
+  updateBook(id: string, book: Book): void {
+    console.log('updating ' + JSON.stringify(book));
+    this.bookService.updateBook(id, book)
+      .subscribe({
+        next: book => {
+          console.log(JSON.stringify(book) + ' has been updated');
+          this.message = " book has been updated";
+        },
+        error: (err) => this.message = err
+      });
+    // so the updated list appears
+
+    this.bookService.getBooks().subscribe({
+      next: (value: Book[]) => this.bookList = value,
+      complete: () => console.log('book service finished'),
+      error: (mess) => this.message = mess
+    })
+  }
+
+
+  /* either the form has closed without saving or new book details have been
+  entered or a book has been updated */
+
+  bookFormClose(book?: Book): void {
+    this.showBookForm = false;
+    console.table(book);
+    if (book == null) {
+      this.message = "form closed without saving";
+      this.currentBook = undefined
+    }
+    else if (this.currentBook == null) {
+      this.addNewBook(book);
+    }
+    else {
+      this.updateBook(this.currentBook._id, book)
+    }
+  }
+
+// note: Bad UX there is no check that the user wants to delete the book and hasn't just 
+// hit the button by mistake
+
+  deleteBook() {
+    console.log('deleting a book ');
+    if (this.currentBook) {
+      this.bookService.deleteBook(this.currentBook._id)
+        .subscribe({
+          next: book => {
+            console.log(JSON.stringify(book) + ' has been added');
+            this.message = "book has been deleted";
+          },
+          error: (err) => this.message = err
+        });
+    }
+
+    // so the updated list appears
+
+    this.bookService.getBooks().subscribe({
+      next: (value: Book[]) => this.bookList = value,
+      complete: () => console.log('book service finished'),
       error: (mess) => this.message = mess
     })
 
   }
-  clicked (book: Book): void {
-    this.currentBook = book;
+
+
+  dismissAlert() {
+    this.message = "";
   }
 
 }
